df5c4fd41f96193e3dbf6f69242a6d2cac830790
Buddy <adityasahai925@gmail.com>
2020-03-15 22:27:56 +0530

    Adding Score

--- snake_game.py
+++ snake_game.py
@@ -16,9 +16,9 @@ black = (0, 0, 0)
 
 game_window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Snake Game')
-#background_image = pygame.image.load('wall.png')
 heading_font = pygame.font.Font('game_over.ttf', 250)
 box_font = pygame.font.Font('game_over.ttf', 75)
+score_font = pygame.font.Font('game_over.ttf',60)
 
 def GenerateApple(apple_size, width, height, wall_thickness, wall_rects):
     apple_x = random.randrange(wall_thickness, width - (apple_size + 1), apple_size)
@@ -70,8 +70,8 @@ def DrawButtons(win, width, height, buttons, color1, color2, box_width, box_heig
         elif not button.collidepoint(mouse_pos):
             pygame.draw.rect(win, color1, button)
 
-def WriteText(win, rectangle, message, font, ):
-    text = font.render(str(message), True, black)
+def WriteText(win, rectangle, message, font, color):
+    text = font.render(str(message), True, color)
     text_rect = text.get_rect()
     text_rect.center = rectangle.center
     win.blit(text, (text_rect[0], text_rect[1]))
@@ -80,20 +80,22 @@ def DrawIntroWindow(win, width, height, buttons, color1, color2, box_width, box_
     win.fill(white)
     heading_y = int(((height / 2) / 2) / 2)
     heading_rect = pygame.Rect(0, 0, width, int(height / 2))
-    WriteText(win, heading_rect, 'Snake', heading_font)
+    WriteText(win, heading_rect, 'Snake', heading_font, black)
     DrawButtons(win, width, height, buttons, color1, color2, box_width, box_height)
-    WriteText(win, buttons[0], 'Play', box_font)
-    WriteText(win, buttons[1], 'Snake Color', box_font)
-    WriteText(win, buttons[2], 'Map Color', box_font)
+    WriteText(win, buttons[0], 'Play', box_font, black)
+    WriteText(win, buttons[1], 'Snake Color', box_font, black)
+    WriteText(win, buttons[2], 'Map Color', box_font, black)
     pygame.display.update()
 
-def DrawGameWindow(win, width, height, bgcolor, snake, snake_size, snake_color, apple_rect, wall_rects):
+def DrawGameWindow(win, width, height, bgcolor, snake, snake_size, snake_color, apple_rect, wall_rects, score):
     win.fill(black)
     for wall in wall_rects:
         pygame.draw.rect(win, blue, wall)
     pygame.draw.rect(win, red, apple_rect)
     for box in snake:
         pygame.draw.rect(win, snake_color, (box[0], box[1], snake_size, snake_size))
+    score_rect = wall_rects[1]#pygame.Rect(width - int(width / 3), height - int(height / 5), int(width / 4), int(height / 4))
+    WriteText(win, score_rect, 'Score: ' + str(score), score_font, white)
     pygame.display.update()
 
 button_box_width = 300
@@ -106,10 +108,10 @@ button_list = [
 
 running = True
 game_over = True
-pressed_button = 0#None
+pressed_button = None
 
 clock = pygame.time.Clock()
-'''
+
 while running:
 
     for event in pygame.event.get():
@@ -120,7 +122,7 @@ while running:
     DrawIntroWindow(game_window, width, height, button_list, green, red, button_box_width, button_box_height)
 
     clock.tick(10)
-'''
+
 if pressed_button == 0:
     game_over = False
 
@@ -162,14 +164,12 @@ while not game_over:
             elif event.key == pygame.K_RIGHT:
                 y_change = 0
                 x_change = snake_size
-            elif event.key == pygame.K_SPACE:
-                snake_length += 1
 
     snake_x, snake_y, snake_list, game_over = MoveSnake(snake_list, x_change, y_change, snake_x, snake_y, snake_length, snake_size, wall_rects)
     snake_head_rect = pygame.Rect(snake_x, snake_y, snake_size, snake_size)
     apple_rect = pygame.Rect(apple_x, apple_y, apple_size, apple_size)
     snake_length, apple_x, apple_y = EatApple(snake_head_rect, apple_rect, width, height, snake_length, wall_thickness, wall_rects)
-    DrawGameWindow(game_window, width, height, black, snake_list, snake_size, green, apple_rect, wall_rects)
+    DrawGameWindow(game_window, width, height, black, snake_list, snake_size, green, apple_rect, wall_rects, snake_length)
 
     clock.tick(10)
 
